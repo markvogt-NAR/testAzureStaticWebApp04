6bd7682d8912f3025e8dda9ef7ce3222bd51a807
Mark Vogt <markevogt@outlook.com>
2024-03-24 09:04:10 -0500

    UPDATED test message...

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Hello from the API"
+        text: " the API endpoint '/message' got auto-called on page load, which returns this Hello MARK from the API"
     });
 };
